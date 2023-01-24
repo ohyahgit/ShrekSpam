4b22cfb849c81be588468d4b39ea4ce54901cce3
ohyahgit <39706781+ohyahgit@users.noreply.github.com>
2023-01-24 17:37:13 +0000

    Pending changes exported from your codespace

--- shrek.py
+++ shrek.py
@@ -3,7 +3,7 @@ from tkinter import *
 while True:
     win=Tk()
     win.title("Shrek")
-    lbl=Label(win, text="Shrek", fg='green', font=("Helvetica", 16))
+    lbl=Label(win, text="Shrek!", fg='green', font=("Helvetica", 16))
     lbl.place(x=60, y=50)
     win.geometry("300x200+10+20")
     win.mainloop()
